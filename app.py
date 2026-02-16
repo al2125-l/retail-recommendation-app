97e74d9e414583e83e19ca01a8f3c4f99a8c8472
alinalakra2 <alinalakra2@gmail.com>
2026-02-16 15:32:17 +0530

    Updated dashboard

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Retail Recommendation System")
-st.write("App is working!")
+st.title("🛍 Retail Recommendation Dashboard")
+
+st.write("Upload customer data to generate personalized offers.")
+
+uploaded_file = st.file_uploader("Upload CSV file")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("Preview of Data")
+    st.dataframe(df.head())
